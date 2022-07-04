e99e8787ae3c64763801c785d7919a13120a70dc
Yoonjiwon <k112751@koreainvestment.com>
2022-07-04 15:24:12 +0900

    Set CollectionLayout
    Create collectionView to apply InfiniteLayout

--- InfiniteLayoutExample/ViewController.swift
+++ InfiniteLayoutExample/ViewController.swift
@@ -6,12 +6,85 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 16.0
+
+        let collectionView = UICollectionView(
+            frame: .zero,
+            collectionViewLayout: layout
+        )
+
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(
+            UICollectionViewCell.self,
+            forCellWithReuseIdentifier: "cell"
+        )
+        collectionView.contentInset = UIEdgeInsets(top: 0, left: 16.0, bottom: 0, right: 0)
+
+        return collectionView
+    }()
+
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+        view.backgroundColor = .systemBackground
+        setupViews()
+    }
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int
+    ) -> Int {
+        return 5
+    }
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: "cell",
+            for: indexPath
+        )
+        cell.backgroundColor = .gray
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAt indexPath: IndexPath
+    ) -> CGSize {
+        return CGSize(width: UIScreen.main.bounds.width - 32, height: collectionView.frame.height)
+    }
+}
+
+private extension ViewController {
+    func setupViews() {
+        [
+            collectionView
+        ]
+            .forEach {
+                view.addSubview($0)
+            }
+
+        collectionView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+            $0.height.equalTo(300.0)
+        }
     }
 }
 

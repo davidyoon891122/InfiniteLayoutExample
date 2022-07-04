88d284cd2e89b88845de97a2aaf0b7597545ac3d
Yoonjiwon <k112751@koreainvestment.com>
2022-07-05 08:37:17 +0900

    Apply Paging with InfiniteLayout
    IsItemPagingEnable has added on InfiniteCollection option for using paging,
    Code Convention has applied on MockNews, values of collectionView are modified

--- InfiniteLayoutExample/TestCollectionViewCell.swift
+++ InfiniteLayoutExample/TestCollectionViewCell.swift
@@ -10,34 +10,44 @@ import SnapKit
 import UIKit
 
 final class TestCollectionViewCell: UICollectionViewCell {
+    static let identifier = "TestCollectionViewCell"
+    
     private lazy var titleLabel: UILabel = {
         let label = UILabel()
         label.textColor = .label
-        label.font = .systemFont(ofSize: 14.0, weight: .bold)
+        label.font = .systemFont(ofSize: 16.0, weight: .bold)
+        label.textColor = .red
         return label
     }()
 
     private lazy var contentLabel: UILabel = {
         let label = UILabel()
         label.textColor = .label
-        label.font = .systemFont(ofSize: 12.0)
+        label.numberOfLines = 2
+        label.font = .systemFont(ofSize: 16.0)
         return label
     }()
 
     private lazy var dateLabel: UILabel = {
         let label = UILabel()
         label.textColor = .label
-        label.font = .systemFont(ofSize: 10.0, weight: .light)
+        label.font = .systemFont(ofSize: 12.0, weight: .light)
         return label
     }()
 
     func setupCell(news: NewsModel) {
         setupViews()
+        titleLabel.text = news.title
+        contentLabel.text = news.content
+        dateLabel.text = news.date
     }
 }
 
 private extension TestCollectionViewCell {
     func setupViews() {
+        backgroundColor = .lightGray
+        layer.cornerRadius = 10
+        layer.masksToBounds = true
         [
             titleLabel,
             contentLabel,
@@ -50,18 +60,20 @@ private extension TestCollectionViewCell {
         titleLabel.snp.makeConstraints {
             $0.top.equalToSuperview().offset(16.0)
             $0.leading.equalToSuperview().offset(16.0)
+            $0.height.equalTo(16.0)
         }
 
         contentLabel.snp.makeConstraints {
             $0.top.equalTo(titleLabel.snp.bottom).offset(8.0)
             $0.leading.equalTo(titleLabel)
             $0.trailing.equalToSuperview().offset(-16.0)
+            $0.height.equalTo(40.0)
         }
 
         dateLabel.snp.makeConstraints {
-            $0.top.equalTo(contentLabel.snp.bottom)
+            $0.top.equalTo(contentLabel.snp.bottom).offset(4.0)
             $0.leading.equalTo(contentLabel)
-            $0.bottom.equalToSuperview().offset(-16.0)
+            $0.bottom.equalToSuperview().offset(-4.0)
         }
     }
 }
--- InfiniteLayoutExample/ViewController.swift
+++ InfiniteLayoutExample/ViewController.swift
@@ -13,8 +13,8 @@ class ViewController: UIViewController {
     private lazy var collectionView: InfiniteCollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
-        layout.minimumLineSpacing = 16
-
+        layout.minimumLineSpacing = 8
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 0)
         let collectionView = InfiniteCollectionView(
             frame: .zero,
             collectionViewLayout: layout
@@ -22,15 +22,44 @@ class ViewController: UIViewController {
 
         collectionView.dataSource = self
         collectionView.delegate = self
+        collectionView.infiniteDelegate = self
         collectionView.register(
-            UICollectionViewCell.self,
-            forCellWithReuseIdentifier: "cell"
+            TestCollectionViewCell.self,
+            forCellWithReuseIdentifier: TestCollectionViewCell.identifier
         )
-        collectionView.isPagingEnabled = true
+//        collectionView.isPagingEnabled = true
+        collectionView.isItemPagingEnabled = true
 
         return collectionView
     }()
 
+    private let mockNews = [
+        NewsModel(
+            title: "삼성전자",
+            content: "반도체 매출 증가에 따른 주가 상승이 보이지 않아, 동학 개미들 이탈 가속",
+            date: "20220704"
+        ),
+        NewsModel(
+            title: "에어부산",
+            content: "에어부산, 지역 항공인재 양성 나선다…지원금도 제공",
+            date: "20220703"
+        ),
+        NewsModel(
+            title: "SK하이닉스",
+            content: "[특징주] 삼성전자·SK하이닉스, 신저가 찍고 4거래일만에 반등(종합)",
+            date: "20220702"
+        ),
+        NewsModel(
+            title: "하나투어",
+            content: "물가 걱정에 기 못 펴는 리오프닝주…하나투어 26%↓ 모두투어 22%↓",
+            date: "20220701"
+        ),
+        NewsModel(
+            title: "미래에셋증권",
+            content: "미래에셋증권, 새로운 MTS 서비스 개시",
+            date: "20220630"
+        )
+    ]
 
 
     override func viewDidLoad() {
@@ -45,29 +74,36 @@ extension ViewController: UICollectionViewDataSource {
         _ collectionView: UICollectionView,
         numberOfItemsInSection section: Int
     ) -> Int {
-        return 5
+        return mockNews.count
     }
 
     func collectionView(
         _ collectionView: UICollectionView,
         cellForItemAt indexPath: IndexPath
     ) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(
-            withReuseIdentifier: "cell",
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: TestCollectionViewCell.identifier,
             for: indexPath
-        )
-        cell.backgroundColor = .gray
+        ) as? TestCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        let news = mockNews[indexPath.row % mockNews.count]
+
+        cell.setupCell(news: news)
         return cell
     }
 }
 
-extension ViewController: UICollectionViewDelegateFlowLayout {
+extension ViewController: UICollectionViewDelegateFlowLayout, InfiniteCollectionViewDelegate {
     func collectionView(
         _ collectionView: UICollectionView,
         layout collectionViewLayout: UICollectionViewLayout,
         sizeForItemAt indexPath: IndexPath
     ) -> CGSize {
-        return CGSize(width: UIScreen.main.bounds.width - 32, height: collectionView.frame.height)
+        return CGSize(
+            width: UIScreen.main.bounds.width - 32,
+            height: 120.0
+        )
     }
 }
 
@@ -84,7 +120,7 @@ private extension ViewController {
             $0.top.equalTo(view.safeAreaLayoutGuide)
             $0.leading.equalToSuperview()
             $0.trailing.equalToSuperview()
-            $0.height.equalTo(150.0)
+            $0.height.equalTo(120.0)
         }
     }
 }
